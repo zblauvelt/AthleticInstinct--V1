a2adcf9840fce90e64e94b2785ebeceea9d818ba
zblauvelt <zblauvelt@hotmail.com>
2017-10-25 21:37:19 -0400

    Commit UI for ScrollView

---
+++ AthleticInstinct/OvalButton.swift
@@ -0,0 +1,31 @@
+//
+//  OvalButton.swift
+//  AthleticInstinct
+//
+//  Created by Zachary Blauvelt on 10/25/17.
+//  Copyright © 2017 ZackBlauvelt. All rights reserved.
+//
+
+import UIKit
+
+class OvalButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        //imageView?.contentMode = .scaleAspectFit
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width * 0.07  
+        
+    }
+
+}
